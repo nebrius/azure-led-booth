0d4aa1a1bed20a9033d4611b338996c32a49d2b6
Bryan Hughes <bryan@nebri.us>
2019-05-17 16:56:57 -0700

    Implemented build and deploy infrastructure for clients

--- functions/deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-npm prune --production
-zip -r deploy.zip * && \
-  az functionapp deployment source config-zip --subscription "CDA Global Demos" --resource-group "brhugh-led" -n "brhugh-led-function-app" --src ./deploy.zip && \
-  rm ./deploy.zip
-npm install
